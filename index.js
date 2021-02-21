e59e6712bd1804690179d83bbef8879ff1a1449e
Chau Tran <cctran96@gmail.com>
2021-02-21 04:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    for (i = 0; i < katzDeliLine.length; i++) {
+    for (let i = 0; i < katzDeliLine.length; i++) {
       line[i] = `${[i+1]}. ${katzDeliLine[i]}`
     }
   }
